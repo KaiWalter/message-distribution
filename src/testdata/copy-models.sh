fc593a83ce6a705a3abebc859daf79e7adc24876
Kai Walter <kai.walter@outlook.de>
2023-08-06 13:58:14 +0000

    fix model copy

--- src/testdata/copy-models.sh
+++ src/testdata/copy-models.sh
@@ -2,9 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cp $SCRIPT_DIR/Models/* ../funcdistributor/Models/
-cp $SCRIPT_DIR/Models/* ../funcrecvexp/Models/
-cp $SCRIPT_DIR/Models/* ../funcrecvstd/Models/
-cp $SCRIPT_DIR/Models/* ../daprdistributor/
-cp $SCRIPT_DIR/Models/* ../daprrecvexp/
-cp $SCRIPT_DIR/Models/* ../daprrecvstd/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../acafdistributor/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../acafrecvexp/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../acafrecvstd/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../funcdistributor/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../funcrecvexp/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../funcrecvstd/Models/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../daprdistributor/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../daprrecvexp/
+cp $SCRIPT_DIR/Models/* $SCRIPT_DIR/../daprrecvstd/
